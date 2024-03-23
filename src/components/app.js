f861d18f4813a4a7ca92911dc7a54bb8852e1323
JLeandroJM <jose.machaca.s@utec.edu.pe>
2024-03-23 00:07:17 -0500

    tercer commit

--- src/components/app.js
+++ src/components/app.js
@@ -12,19 +12,17 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  position: relative; /* Establece posición relativa para poder posicionar los labels */
-  padding: 0 20px;
+  padding: 0 5vw; /* Ajuste de espaciado horizontal */
 `;
 
 const Navbar = styled.nav`
   width: 100%;
-  height: 80px;
-  padding: 10px 20px;
+  height: 10vh; /* Ajustar altura de la barra de navegación */
+  padding: 1vh 2vw; /* Ajustar espaciado dentro de la barra de navegación */
   display: flex;
   justify-content: space-between;
   align-items: center;
   color: white;
-  position: relative;
 `;
 
 const LogoContainer = styled.div`
@@ -33,14 +31,12 @@ const LogoContainer = styled.div`
 `;
 
 const Logo = styled.img`
-  width: 70px;
+  width: 5vw; /* Ajuste de tamaño del logo */
   height: auto;
-  margin-right: 70px;
-  margin-left: 100px;
 `;
 
 const CompanyName = styled.h1`
-  font-size: 40px;
+  font-size: 3vw; /* Ajuste de tamaño de fuente para dispositivos grandes */
   font-weight: bold;
   font-family: 'Berlin Sans FB', sans-serif;
 
@@ -53,15 +49,16 @@ const CompanyName = styled.h1`
   -webkit-text-fill-color: transparent;
 
   @media screen and (max-width: 768px) {
-    font-size: 32px; /* Reducimos el tamaño de fuente para dispositivos móviles */
+    font-size: 3.2vw; /* Reducimos el tamaño de fuente para dispositivos móviles */
   }
 `;
 
+
 const Label2 = styled.h1`
-  font-size: 50px;
+  font-size: 4vw; /* Ajuste de tamaño de fuente para dispositivos grandes */
   font-weight: bold;
   font-family: 'Berlin Sans FB', sans-serif;
-  color:white;
+  color: white;
   @font-face {
     font-family: 'Berlin Sans FB';
     src: url(${berlinSansFB}) format('truetype');
@@ -75,26 +72,27 @@ const Label2 = styled.h1`
   ${({ bottom }) => (bottom && `bottom: ${bottom};`)}
 
   @media screen and (max-width: 768px) {
-    font-size: 40px; /* Reducimos el tamaño de fuente para dispositivos móviles */
+    font-size: 3.5vw; /* Reducimos el tamaño de fuente para dispositivos móviles */
   }
 `;
 
+
 const StyledImage = styled.img`
-  width: 85%;
-  max-width: 85%;
+  width: 90%;
+  max-width: 90%;
   height: auto;
 `;
 
 const Label = styled.h2`
-  font-size: 60px;
+  font-size: 4vw; /* Ajuste de tamaño de fuente para dispositivos grandes */
   font-weight: bold;
   font-family: 'Eras Bold ITC', sans-serif;
-  color:white;
+  color: white;
   @font-face {
     font-family: 'Eras Bold ITC';
     src: url(${ErasBD}) format('truetype');
   }
-  
+
   position: absolute;
   /* Estilos condicionales para ajustar la posición */
   ${({ top }) => (top && `top: ${top};`)}
@@ -103,64 +101,37 @@ const Label = styled.h2`
   ${({ bottom }) => (bottom && `bottom: ${bottom};`)}
 
   @media screen and (max-width: 768px) {
-    font-size: 48px; /* Reducimos el tamaño de fuente para dispositivos móviles */
+    font-size: 3.5vw; /* Reducimos el tamaño de fuente para dispositivos móviles */
   }
 `;
+
 
 
 
 
 
 const NavLink = styled.a`
-  font-size: 18px;
+  font-size: 1.5vw; /* Ajuste de tamaño de fuente para enlaces de navegación */
   font-family: 'Berlin Sans FB', sans-serif;
   color: black;
-  margin: 0 10px;
+  margin: 0 1vw; /* Ajuste de espaciado horizontal */
   text-decoration: none; /* Evita que los enlaces se subrayen */
-
-  /* Estilos condicionales para ajustar la posición */
-  ${props => props.top && css`
-    position: absolute;
-    top: ${props.top};
-  `}
-  ${props => props.left && css`
-    position: absolute;
-    left: ${props.left};
-  `}
-  ${props => props.right && css`
-    position: absolute;
-    right: ${props.right};
-  `}
-  ${props => props.bottom && css`
-    position: absolute;
-    bottom: ${props.bottom};
-  `}
-
-  @media screen and (max-width: 768px) {
-    font-size: 14px;
-  }
-
   &:hover {
-    font-size: 20px; /* Tamaño aumentado al pasar el mouse */
+    font-size: 26px; /* Tamaño aumentado al pasar el mouse */
   }
 `;
 
 
 const Button = styled.button`
-  font-size: 30px;
-  font-family: 'Berlin Sans FB', sans-serif;
-  color: white;
-  background-color: #17B1F4;
-  border: none;
-  border-radius: 20px;
-  padding: 10px 20px;
-  cursor: pointer;
-  position: absolute;
-  top: 57%;
-  left: 26%;
-  transform: translate(-50%, -50%);
-  transition: font-size 0.3s ease; /* Agrega una transición para suavizar el efecto de cambio de tamaño */
-
+font-size: 2vw; /* Ajuste de tamaño de fuente para el botón */
+font-family: 'Berlin Sans FB', sans-serif;
+color: white;
+background-color: #17B1F4;
+border: none;
+border-radius: 2vw; /* Ajuste del radio del borde del botón */
+padding: 1vh 2vw; /* Ajuste del espaciado dentro del botón */
+cursor: pointer;
+transition: font-size 0.3s ease;
   &:hover {
     font-size: 32px; /* Tamaño aumentado al pasar el mouse */
     text-decoration: none; /* Evita que el texto se subraye */
@@ -181,10 +152,10 @@ function App() {
           <CompanyName>CLEAN & GO</CompanyName>
         </LogoContainer>
         <div>
-          <NavLink href="#" top="50%" left="53%">HOME</NavLink>
-          <NavLink href="#" top="50%" left="61%">SERVICES</NavLink>
-          <NavLink href="#" top="50%" left="70%">ABOUT US</NavLink>
-          <NavLink href="#" top="50%" left="80%">CONTACT US</NavLink>
+          <NavLink href="#1" top="50%" left="50%">HOME</NavLink>
+          <NavLink href="#2" top="50%" left="59%">SERVICES</NavLink>
+          <NavLink href="#3" top="50%" left="70%">ABOUT US</NavLink>
+          <NavLink href="#4" top="50%" left="80%">CONTACT US</NavLink>
         </div>
       </Navbar>
       <StyledImage src={landing} alt="Mi imagen" />
@@ -193,9 +164,7 @@ function App() {
       <Label2 top="28%" left="22%">WITH</Label2>
       <Label top="35%" left="13%">CLEANING?</Label>
       <Button>CONTACT</Button> {/* Botón añadido */}
-      <MainContent>
-        {/* Contenido de la página */}
-      </MainContent>
+      
     </Container>
   );
 }
